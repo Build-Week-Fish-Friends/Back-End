66714ed5412582bc57419e44fbe46217b122fd23
Ben <thisbenrogers@gmail.com>
2019-11-17 00:17:01 -0600

    :lock: Adds login and register endpoints
    Returns JWT

--- API/server.js
+++ API/server.js
@@ -4,8 +4,9 @@ const helmet = require('helmet');
 
 const logger = require('../middleware/logger');
 
-// TODO declare routers
 const usersRouter = require("../users/user-router");
+const loginRouter = require("../auth/login-router.js");
+const registerRouter = require("../auth/register-router.js");
 
 const server = express();
 
@@ -14,7 +15,8 @@ server.use(logger);
 server.use(express.json());
 server.use(cors());
 
-// TODO user() routers
+server.use("/api/login", loginRouter);
+server.use("/api/register", registerRouter);
 server.use("/api/users", usersRouter);
 
 server.get('/', (req, res) => {
