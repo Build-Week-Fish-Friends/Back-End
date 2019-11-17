38b162ceff41b7a4b121c8c4ab87f6962bb51f80
Ben <thisbenrogers@gmail.com>
2019-11-16 20:24:16 -0600

    :sparkles: Adds server endpoint
    Fixes error in server.listen in index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const server = require('./API/server.js');
 
 const port = process.env.PORT || 5000;
-server.listen(`\n<<< Server running on port ${port} >>>\n`)
+server.listen(port, () => {
+  console.log(`\n<<< Server running on port ${port} >>>\n`);
+})
