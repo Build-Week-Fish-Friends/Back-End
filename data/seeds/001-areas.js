12bac8ede70dc69dc3b12f3637523ee9973280d8
Ben <thisbenrogers@gmail.com>
2019-11-18 16:23:40 -0600

    :clown_face: Seeds areas table

---
+++ data/seeds/001-areas.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('areas').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('areas').insert([
+        {id: 1, name: 'North Muddy Boggy Creek'},
+        {id: 2, name: 'South Muddy Boggy Creek'},
+        {id: 3, name: 'East Lake Minnetonka'},
+        {id: 4, name: 'West Lake Minnetonka'}
+      ]);
+    });
+};
