a92d9f85281251d33227d47fb2c4f37e8ee34133
Ben <thisbenrogers@gmail.com>
2019-11-19 12:02:45 -0600

    :sparkles: Adds CRUD to /logs endpoints
    Deploys to staging for testing on heroku

--- fishLogs/log-router.js
+++ fishLogs/log-router.js
@@ -27,4 +27,95 @@ router.get('/:id', restricted, (req, res) => {
   }
 });
 
+
+router.get('/user/:id', (req, res) => {
+  const user_id = req.params.id;
+  // res.status(200).json({ user_id: user_id });
+  if (!user_id) {
+    res.status(400).json({ message: 'This user does not seem to exist' });
+  } else {
+    Logs.findBy({ user_id })
+      .then(logs => {
+        res.status(201).json(logs)
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'The users logs could not be retrieved' })
+      })
+  }
+})
+
+
+
+router.get('/area/:id', (req, res) => {
+  const area_id = req.params.id;
+  // res.status(200).json({ area_id: area_id });
+  if (!area_id) {
+    res.status(400).json({ message: 'This area does not seem to exist' });
+  } else {
+    Logs.findBy({ area_id })
+      .then(logs => {
+        res.status(201).json(logs)
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'The areas logs could not be retrieved' })
+      })
+  }
+})
+
+router.post('/', (req, res) => {
+  let log = req.body;
+
+    Logs.add(log)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      })
+})
+
+
+router.put('/:id', (req, res) => {
+  
+  const { id } = req.params;
+  const changes = req.body;
+
+  Logs.findById(id)
+    .then(log => {
+      if (log) {
+        Logs.update(id, changes)
+          .then(updatedLog => {
+            res.status(200).json(updatedLog);
+          });
+      } else {
+        res.status(404).json({ message: 'Could not find log with the given id'});
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ 
+        message: 'Failed to update log',
+      });
+    })
+})
+
+
+// TODO remove(id) - returns number of logs removed
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Logs.remove(id)
+    .then(deleted => {
+      if (deleted) {
+        res.json({ removed: deleted });
+      } else {
+        res.status(404).json({ message: 'Could not find log with given id' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to delete log' });
+    })
+})
+
+
+
 module.exports = router;
