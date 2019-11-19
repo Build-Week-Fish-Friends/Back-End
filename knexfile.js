2903e071fe9e20d9feffac7d23c899488b788c9d
Ben <thisbenrogers@gmail.com>
2019-11-18 18:12:30 -0600

    :wrench: Changes development db
    Switches from SQLite3 to PostgreSQL

--- knexfile.js
+++ knexfile.js
@@ -3,21 +3,17 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/fishFriends.db3',
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
+    client: 'pg',
+    connection: 'postgres://localhost/fishFriends',
     migrations: {
       directory: './data/migrations',
     },
     seeds: {
       directory: './data/seeds',
+    },
+    pool: {
+      min: 2,
+      max: 10,
     },
   },
 
